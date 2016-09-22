281c66e2aba8c1fb89ab5e21b5aae04942be1955
Kzmaster <kzanders@mcad.edu>
2016-09-21 19:02:58 -0500

    Initial Commit

---
+++ assignment/script.js
@@ -0,0 +1,60 @@
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Donate" button.
+});
+
+document.getElementById('BtnChurchill').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Churchill" button.
+});
+
+document.getElementById('BtnGhandi').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Ghandi" button.
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Demosthenes" button.
+});
+
+var churchill = (
+	'name': "Winston Churchill",
+	'year': 1940,
+	'BCE': false,
+
+
+)
+
+var ghandi = (
+	'name': 'Ghandi',
+	'year': 1942,
+	'BCE': false,
+)
+
+var philip = (
+	'name': 'The Third Phillipic',
+	'year': 342,
+	'BCE': true,
+)
+
+var speecharray = [['Winston Churchill', 1940, false], ['Ghandi', 1942, false], [
+'The Third Phillipic', 342, true]]
+
+var difference = parseInt(ghandi[1]) - parseInt(churchill[1])
+
+function years() { 
+	console.log("Ghandi's speech and Churchill's speech are (difference) years apart.")
+}
+
+function display() {
+	console.log("There are (speecharray.length) speeches on the page.")
+}
+
+function infochurchill() {
+	console.log("This speech was written by (churchill.name) in (churchill.year). It is (churchill.BCE) that this year is B.C.E.")
+}
+
+function infoghandi() {
+	console.log("This speech was written by (ghandi.name) in (ghandi.year). It is (ghandi.BCE) that this year is B.C.E.")
+}
+
+function infophilip() {
+	console.log("This speech was written by (philip.name) in (philip.year). It is (philip.BCE) that this year is B.C.E.")
+}
